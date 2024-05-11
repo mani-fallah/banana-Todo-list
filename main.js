4abd82e2111c0dcdcd40de56c167eadb473be142
mani-fallah <mani.f.s.2004@gmail.com>
2024-05-11 13:40:14 +0330

    save and delete added

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const listEl =document.querySelector("#tasks");
 const titleTag = document.title;
 const body = document.body;
 const modeIcon = document.querySelector('#ModeIcon');
+let allToDos = ['play footbal'];
+
+document.addEventListener('DOMContentLoaded',(evt) => {
+    allToDos = getToDos();
+    console.log(allToDos);
+    displayList(allToDos);
+})
 window.addEventListener("blur",()=>{
     document.title = "dont forget to do your list";
 });
@@ -12,10 +19,11 @@ window.addEventListener("focus",()=>{
     document.title = titleTag;
 });
 
+
+
+
+
 window.addEventListener('load',()=>{
-
-
-
     form.addEventListener('submit',(e) => {
         e.preventDefault();
         const task = input.value;
@@ -23,6 +31,7 @@ window.addEventListener('load',()=>{
         if(!task)
           alert("please fill out this input")
         else {
+            allToDos.push(input.value);
 
             input.value = ''
 
@@ -85,12 +94,23 @@ window.addEventListener('load',()=>{
             });
 
             taskdeleteEl.addEventListener('click',()=>{
+                for (let i = 0;i< allToDos.length;i++)
+                {
+                    if (allToDos[i] === document.querySelector('.text').value)
+                    {
+                        allToDos = allToDos.filter(function(item) {
+                            return item !== document.querySelector('.text').value
+                        })
+                    }
+                }
+                localStorage.setItem("todos",JSON.stringify(allToDos))
                 listEl.removeChild(taskEl);
             });
 
 
         }
 
+        localStorage.setItem("todos",JSON.stringify(allToDos))
     });
 });
 
@@ -106,7 +126,7 @@ modeIcon.addEventListener('click',()=>{
         input.style.color = 'var(--dark)';
         let e =document.getElementsByClassName('task');
         let v =document.getElementsByClassName('text');
-        for(let i =0 ; i<e.length;i++)
+        for(let i =0 ; i<= e.length;i++)
         {
             e[i].style.backgroundColor = '#e0e5ec';
             v[i].style.color = 'var(--dark)'
@@ -130,8 +150,90 @@ modeIcon.addEventListener('click',()=>{
             v[i].style.color = 'var(--light)'
         }
     }
-
-
-
-
 })
+
+function displayList(todos)
+{
+
+    for(let i =0;i < todos.length;i++)
+    {
+    if(todos[i])
+    {
+        const taskEl = document.createElement("div");
+        taskEl.classList.add("task");
+
+        const taskContetnEl = document.createElement("div");
+        taskContetnEl.classList.add('content');
+
+        taskEl.appendChild(taskContetnEl);
+        const taskInputEl = document.createElement('input');
+        taskInputEl.classList.add('text');
+        taskInputEl.value= todos[i];
+        taskInputEl.setAttribute("readonly","readonly")
+        taskInputEl.type ="text";
+        taskContetnEl.appendChild(taskInputEl);
+
+        const taskActionEl = document.createElement('div');
+        taskActionEl.classList.add('actions');
+
+        const taskEditEl = document.createElement('button');
+        taskEditEl.classList.add('edit');
+        taskEditEl.name = "Edit";
+        const EditIcon = document.createElement('i');
+        EditIcon.classList.add('fas');
+        EditIcon.classList.add('fa-edit');
+        taskEditEl.appendChild(EditIcon);
+
+        const taskdeleteEl = document.createElement('button');
+        taskdeleteEl.classList.add('delete');
+        const deletIcon = document.createElement('i');
+        deletIcon.classList.add('fas');
+        deletIcon.classList.add('fa-trash');
+        taskdeleteEl.appendChild(deletIcon);
+
+
+
+        taskActionEl.appendChild(taskEditEl)
+        taskActionEl.appendChild(taskdeleteEl)
+        taskEl.appendChild(taskActionEl)
+
+
+        listEl.appendChild(taskEl);
+
+        taskEditEl.addEventListener('click',()=>{
+            if(taskEditEl.name === "Edit"){
+                taskInputEl.removeAttribute("readonly");
+                taskInputEl.focus();
+                EditIcon.classList.add('fas');
+                EditIcon.classList.add('fa-save');
+                EditIcon.classList.remove('fa-edit');
+                taskEditEl.name = "save"
+            }
+            else {
+                taskInputEl.setAttribute("readonly","readonly");
+                EditIcon.classList.remove('fa-save');
+                EditIcon.classList.add('fa-edit');
+                taskEditEl.name = "Edit";
+            }
+        });
+
+        taskdeleteEl.addEventListener('click',()=>{
+            for (let i = 0;i< todos.length;i++)
+            {
+                allToDos = todos.filter(function(item) {
+                    return item !== document.querySelector('.text').value
+                })
+            }
+            localStorage.setItem("todos",JSON.stringify(allToDos))
+            listEl.removeChild(taskEl);
+        });
+    }
+    }
+
+
+}
+function getToDos() {
+    let savedToDos;
+    savedToDos =JSON.parse(localStorage.getItem('todos'))
+    return savedToDos;
+}
